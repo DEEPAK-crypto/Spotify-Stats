79ceda27e4510f32b517f91766475d66890feaf0
Deepak Singh <deepak0064.cse19@chitkara.edu.in>
2020-09-09 01:45:40 +0530

    fixed async function

--- app.js
+++ app.js
@@ -14,14 +14,15 @@ var client_secret = 'be918ee525ee453a8e4821689ee1c27f';
 var redirect_uri = 'http://localhost:3000/logedIn';
 
 app.get('/', function(req, res) {
-	res.render('index');
+	res.redirect('/login');
 });
 
 var authInfo = {};
 var currentUser = {};
 
 app.get('/login', function(req, res) {
-	var scopes = 'user-read-private user-read-email';
+	var scopes =
+		'user-read-private user-read-email user-read-recently-played user-top-read user-follow-read user-follow-modify playlist-read-private playlist-read-collaborative playlist-modify-public';
 	res.redirect(
 		'https://accounts.spotify.com/authorize' +
 			'?response_type=code' +
@@ -52,25 +53,93 @@ app.get('/logedIn', function(req, res) {
 			(authInfo.access_token = body.access_token), (authInforefresh_token = body.refresh_token);
 
 			// use the access token to access the Spotify Web API
-			var options = {
-				url: 'https://api.spotify.com/v1/me',
+			// var options = {
+			// 	url: 'https://api.spotify.com/v1/me',
+			// 	headers: { Authorization: 'Bearer ' + authInfo.access_token },
+			// 	json: true
+			// };
+			const headers = {
 				headers: { Authorization: 'Bearer ' + authInfo.access_token },
 				json: true
 			};
-			request.get(options, function(error, response, body) {
-				currentUser.user = body;
-			});
-			options.url = 'https://api.spotify.com/v1/me/playlists';
 
-			request.get(options, function(error, response, body) {
-				currentUser.playlist = body;
-				res.redirect('/profile');
+			// const user = axios.get('https://api.spotify.com/v1/me', { headers });
+
+			// console.log(user);
+			const user = axios({
+				url: 'https://api.spotify.com/v1/me',
+				headers: { Authorization: 'Bearer ' + authInfo.access_token },
+				json: true
 			});
+			const track = axios({
+				url: 'https://api.spotify.com/v1/me/top/tracks?&limit=10',
+				headers: { Authorization: 'Bearer ' + authInfo.access_token },
+				json: true
+			});
+			const artist = axios({
+				url: 'https://api.spotify.com/v1/me/top/artists?&limit=50',
+				headers: { Authorization: 'Bearer ' + authInfo.access_token },
+				json: true
+			});
+
+			async function data() {
+				await axios
+					.all([ user, track, artist ])
+					.then(function(response) {
+						currentUser.user = response[0].data;
+						currentUser.tracks = response[1].data;
+						currentUser.artist = response[2].data;
+						res.redirect('/profile');
+					})
+					.catch(function(error) {
+						return error;
+					});
+			}
+			data();
+
+			// 	async function user() {
+			// 		currentUser.user = await axios
+			// 			.get('https://api.spotify.com/v1/me', headers)
+			// 			.then(function(response) {
+			// 				return response.data;
+			// 			})
+			// 			.catch(function(error) {
+			// 				return error;
+			// 			});
+			// 	}
+			// 	async function track() {
+			// 		currentUser.tracks = await axios
+			// 			.get('https://api.spotify.com/v1/me/top/tracks?&limit=10', headers)
+			// 			.then(function(response) {
+			// 				res.redirect('/profile');
+			// 				return response.data;
+			// 			})
+			// 			.catch(function(error) {
+			// 				return error;
+			// 			});
+			// 	}
+
+			// 	async function artist() {
+			// 		currentUser.artist = await axios
+			// 			.get('https://api.spotify.com/v1/me/top/artists?&limit=50', headers)
+			// 			.then(function(response) {
+			// 				res.redirect('/profile');
+			// 				return response.data;
+			// 			})
+			// 			.catch(function(error) {
+			// 				return error;
+			// 			});
+			// 	}
+
+			// 	user();
+			// 	track();
+			// 	artist();
 		}
 	});
 });
 
 app.get('/profile', function(req, res) {
+	console.log(currentUser);
 	res.render('profile', { user: currentUser.user, playlist: currentUser.playlist });
 });
 app.listen(3000);
